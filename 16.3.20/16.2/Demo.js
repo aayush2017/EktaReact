8e2d1eec4bda7d057a3b95e65d00024f6109aa1d
AJ <aayush.jaiswal984@gmail.com>
2020-03-16 19:04:40 +0530

    16.2.20

---
+++ 16.3.20/16.2/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Demo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello From Demo</h1>
+            </div>
+        )
+    }
+}
+export default Demo;
---
+++ 16.3.20/16.2/Sample.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Sample() {
+    return (
+        <div>
+            <h1>Hello from Sample</h1>
+        </div>
+    )
+}
+export default Sample;
